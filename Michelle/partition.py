e5d128c2c3cb782d6c70dbe6f7a8dae3e191a860
mkwok <mkwok2@nd.edu>
2023-05-30 18:00:23 -0400

    added my file

---
+++ Michelle/partition.py
@@ -0,0 +1,58 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from updated_generate_data.py import *
+
+naive_data = generate_naive_data(l_bound = -1., h_bound = 1., r0_xmin = -0.15, r0_xmax = 0.15, layers = 5, radii = 5., size = 150)
+plot(naive_data, radii = 5.0)
+sorted_data = np.sort(naive_data)
+
+def repeated(covers):
+    
+    acover = np.zeros((1, 5, 16))
+    loops = len(covers) - 1
+    mins = []
+    print(loops)
+
+    for i in np.arange(5):
+        amin = (covers[loops, i, 15]/(5*(i+1)))
+        mins.append(amin)
+
+    min_index = np.argmin(np.array(mins))
+    #find closest points for all other rows
+    for i in np.arange(5):
+        closest_index = np.argmin(np.abs(sorted_data[i]/(5*(i+1)) - covers[loops, min_index, 15]/(min_index+1)/5))
+        
+        if closest_index >= 150 - 16:
+            acover[0, :, 0:16] = sorted_data[:, -17:-1]
+            covers = np.append(covers, acover, axis = 0)
+            return covers
+        
+        else:
+            acover[0, i, 0:16] = sorted_data[i, closest_index-1:closest_index+15] 
+
+
+        
+    covers = np.append(covers, acover, axis = 0)
+    
+    return repeated(covers)
+    
+    
+def generate_cover(data):
+    covers = np.zeros((1, 5, 16))
+    xshape = np.zeros_like(data[0, 42:58])
+                    
+    #generate first cover
+    for row in np.arange(5):
+        #min_index = np.argmin(np.abs(sorted_data[row]))
+        covers[0, row, 0:16] = data[row, 0:16]
+        #repeated(data, covers)
+
+    return repeated(covers)
+
+colorset = ['orange', 'k', 'b', 'r', 'y', 'm']
+final = generate_cover(sorted_data)
+for j in range(len(final)):
+    for i in range(5):
+        plt.scatter(final[j, i, :], np.full_like(final[j, i, :], (i+1)*5), s = 10, color = colorset[j%6])
+plt.xlim(-1, 1)
+plt.ylim(0, 26)
---
+++ Michelle/updated_generate_data.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_naive_data(l_bound = -1., h_bound = 1., r0_xmin = -0.15, r0_xmax = 0.15, layers = 5, radii = 5., size = 100):
+    n_slope = (radii * layers) / (l_bound - r0_xmin)
+    p_slope = (radii * layers) / (h_bound - r0_xmax)
+
+    fin_arr = np.array([])
+
+    for i in range(1, layers + 1):
+        cur_y_coord = radii * i
+
+        cur_xmin = (cur_y_coord - r0_xmin) / n_slope
+        cur_xmax = (cur_y_coord - r0_xmax) / p_slope
+
+        arr = np.random.uniform(cur_xmin, cur_xmax, size)
+
+        if i == 1:
+            fin_arr = arr
+        else:
+            fin_arr = np.vstack((fin_arr, arr))
+
+    return fin_arr
+
+def plot(arr, radii = 5.0):
+    num_pnts = 5 * np.ones(arr.shape[1])
+    
+    for i in range(arr.shape[0]):
+        plt.scatter(arr[i], num_pnts, s=2, c="b")
+        num_pnts += 5 * np.ones(arr.shape[1])
+    
+    plt.yticks(np.arange(0, num_pnts[0] + 1, radii))
+    
+    plt.show()
+
+naive_data = generate_naive_data(l_bound = -1., h_bound = 1., r0_xmin = -0.15, r0_xmax = 0.15, layers = 5, radii = 5., size = 100)
+plot(naive_data, radii = 5.0)
+
+# np.savetxt("updated_data_150_points.csv", naive_data, delimiter = ",")
