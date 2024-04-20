8f36239a8ad036c43ebb5cc2c7b0894f4cd1f7da
zakk-h <zakkh333@gmail.com>
2024-04-20 12:34:21 -0400

    Main method

--- C/main.c
+++ C/main.c
@@ -9,42 +9,10 @@ void ProcessEvent()
 }
 
 int main() {
-    int numEventsLoaded = 0;
+    int wedgesToTest[] = {24, 25}; 
+    int wedge_count = 2;
 
-    G_stats.count_events = 0;
-    G_stats.count_points = 0;
+    wedge_test("makePatches_ShadowQuilt_fromEdges", 0, 0.5, 16, 15.0, wedgesToTest, wedge_count, 1000, "v3", 50, 15.0, false, false, "Analytic", false, false, false, 6, 3);
 
-	while (Event_load(&G_event) > 0)
-	{
-			ProcessEvent();
-	}
-
-    printf("Events: %d, Points: %d\n", G_stats.count_events, G_stats.count_points);
-
-	assist();
     return 0;
-}
-
-int assist() {
-	const char* lining = "makePatches_Projective_center";
-    float apexZ0 = 0.0;
-    float z0_spacing = 0.5;
-    int ppl = 16;
-    float z0_luminousRegion = 15.0;
-    int wedges[] = {0, 128};  
-    int wedge_count = 2; //array size
-    int lines = 1000;
-    const char* v = "v3";
-    float top_layer_cutoff = 50.0;
-    float accept_cutoff = 10.0;
-    bool leftRightAlign = true;
-    bool uniform_N_points = false;
-    const char* acceptance_method = "Analytic";
-    bool show_acceptance_of_cover = false;
-    bool movie = false;
-    bool savefig = false;
-    int figSizeScale = 6;
-    int movieFigSizeScale = 3;
-	wedge_test(lining, apexZ0, z0_spacing, ppl, z0_luminousRegion, wedges, wedge_count, lines, v, top_layer_cutoff, accept_cutoff, leftRightAlign, uniform_N_points, acceptance_method, show_acceptance_of_cover, movie, savefig, figSizeScale, movieFigSizeScale);
-	return 0;
 }
