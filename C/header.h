471b1da1b998be874edf06db187618a6c94d6236
zakk-h <zakkh333@gmail.com>
2024-05-12 14:46:49 -0400

    Removed methods and structures not going on FPGAs

--- C/header.h
+++ C/header.h
@@ -88,14 +88,6 @@ typedef struct
 
 typedef struct
 {
-    Environment *env;
-    float start;
-    float slope_ll;
-    float slope_ul;
-} LineGenerator;
-
-typedef struct
-{
     index_type layer_num;
     float pSlope;
 
@@ -107,21 +99,6 @@ typedef struct
     float z1_min;
     float z1_max;
 } Parallelogram;
-
-/*
-typedef struct {
-    int layer_num;
-    float pSlope;
-
-    float shadow_topR_jL;
-    float shadow_topR_jR;
-    float shadow_topL_jL;
-    float shadow_topL_jR;
-
-    float top_layer_zmin;
-    float top_layer_zmax;
-} Parallelogram_v1;
-*/
 
 typedef struct
 {
@@ -190,17 +167,13 @@ extern void initDataSetExtra(DataSet *ds, Environment *envI);
 extern void importData(DataSet *ds, Point *data_array, int data_array_size);
 extern void addBoundaryPoint(DataSet *ds, float offset);
 extern void initLine(Line *line, Environment *envI, float start, float slopeI);
-extern void initLineGenerator(LineGenerator *lg, Environment *envI, float startI);
-extern void generateEvenGrid(LineGenerator *lg, Line *lines, int n);
 extern void initWedgeSuperPoint(wedgeSuperPoint *wsp, Point *points, int pointCount);
 extern int areWedgeSuperPointsEqual(wedgeSuperPoint *wsp1, wedgeSuperPoint *wsp2);
 extern void initParallelogram(Parallelogram *pg, int layer_numI, float z1_minI, float z1_maxI, float shadow_bottomL_jRI, float shadow_bottomR_jRI, float shadow_bottomL_jLI, float shadow_bottomR_jLI, float pSlopeI);
-// extern void init_parallelogram_v1(Parallelogram_v1 *pg, int layer_numI, float top_layer_zminI, float top_layer_zmaxI, float shadow_topR_jLI, float shadow_topR_jRI, float pSlopeI);
 extern void wedgePatch_init(wedgePatch *wp, Environment *envI, wedgeSuperPoint *superpointsI, int superpoint_count, float apexZ0I);
 extern float straightLineProjectorFromLayerIJtoK(wedgePatch *wp, float z_i, float z_j, int i, int j, int k);
 extern float straightLineProjector(float z_top, float z_j, int j, Environment *env);
 extern void getParallelograms(wedgePatch *wp);
-// extern void getParallelograms_v1(wedgePatch* wp);
 extern void getShadows(wedgePatch *wp, float zTopMin, float zTopMax);
 extern void get_acceptanceCorners(wedgePatch *wp);
 extern void get_end_layer(wedgePatch *wp);
