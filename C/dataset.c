4b6a21604dae01a7cdc7e86fce9a8ce68c719c46
zakk-h <zakkh333@gmail.com>
2024-05-24 19:26:45 -0400

    Intelligent sort optimization knowing the array is sorted except for two boundary points, guarenteed not to be equal

--- C/dataset.c
+++ C/dataset.c
@@ -14,7 +14,7 @@ void importData(DataSet *ds, Point *data_array, int data_array_size)
     {
         index_type layer = data_array[i].layer_num - 1;
         ds->array[layer][ds->n_points[layer]+1] = data_array[i];
-        ds->n_points[layer]++;
+        ds->n_points[layer]++; //here n_points is not counting the blank spot at index 0. 
         
     }
     // iterating over the layers in DataSet
@@ -29,13 +29,8 @@ void addBoundaryPoint(DataSet *ds, float offset)
 {
     ds->boundaryPoint_offset = offset;
 
-    for (index_type i = 0; i < num_layers; i++)
-    { // num_layers is trapezoid_edges.size(), see environment.c
-        // there needs to be room to add the additional points
-        // shifting the array to make room for the new point at the beginning. That first point is left uninitialized
-        // parameters: (address of where to start writing data, address of what to put where you start writing, number of bytes that need to be moved)
-        //memmove(&ds->array[i][1], &ds->array[i][0], ds->n_points[i] * sizeof(Point));
-
+    for (index_type i = 0; i < num_layers; i++) {
+        //adding two boundary points in each layer
         // inserting at the beginning
         ds->array[i][0].layer_num = i + 1;
         ds->array[i][0].radius = (i + 1) * 5;
@@ -49,23 +44,45 @@ void addBoundaryPoint(DataSet *ds, float offset)
         ds->array[i][lastIndex].phi = ds->array[i][1].phi; // 1 gets what was originally the index 0, but is not index 1 due to the insertion.
         ds->array[i][lastIndex].z = trapezoid_edges[i]; //here we want x.0001
 
+        //now factors in the addition of both boundary points because n_points previously was counting true point additions, and did not count the blank index 0.
         ds->n_points[i] += 2;    
-    }
 
-    //index_type total = 0;
-    // iterating over the layers in DataSet
-    for (index_type i = 0; i < num_layers; i++)
-    {
-        // like before, sorting the points in the ith layer
-        //it is worth noting that the sort could be made more efficient because it is assumed all but two points are sorted, 
-        //and they are at the endpoints. we just need to fit the endpoints where they should go. and they should be the min and max point anyway
-        qsort(ds->array[i], ds->n_points[i], sizeof(Point), comparePoints); 
-        // our code ensures n_points stays up to date and fresh and doesn't need to be recomputed here
-        //total += ds->n_points[i]; // summing sizes of 1D array
+        // adjusting positions using insertion sort techniques as opposed to sorting the entire array. 
+        // we have the guarentee from importData that the array was sorted
+        // assigned points to indices first to avoid risk of comparing uninitialized "blank" points.
+        // as opposed to full sorting algorithms like mergesort, each call here is O(N) and has the potential to escape much earlier. 
+        adjustPointPositionFront(ds->array[i], ds->n_points[i], 0); // adjust the start boundary
+        adjustPointPositionBack(ds->array[i], ds->n_points[i], lastIndex); // adjust the end boundary
     }
-    //ds->total_points = total; // size of 2D array, giving total number of points in the dataset
-    //should this actually be num_layers, not total? .size for a 2D array would give you just the number of rows [if you think of it with matrix convention]
-    //total_points isn't even used, don't need it.
-    //ds->total_points = num_layers;
 
 }
+
+void adjustPointPositionFront(Point *array, int n_points, int start_index) {
+    // move the point at start_index to its correct position to maintain sorted order
+    Point toInsert = array[start_index];
+    int j = start_index;
+    //by checking if j < n_points-2, we are not going to the last index, thus, we will never have a situation where the end boundary point gets shifted before we position it
+    //we check n_points-2 instead of n_points-1 because we have j+1 logic, j is the baseline and the comparison is with the next index, so we need j+1 to be not the end, but 1 away from it.
+    //this is a valid cutoff because the z is the primary (first [and only in the case of the implementation, non-debugging comparator]) comparison in the comparator, and the trapezoid edges are always positive integers, so -x < x when x is a positive integer. 
+    //it cannot be 0, so there is no possible equality as well, which could affect the debugging comparator.
+    while (j < n_points - 2 && comparePoints(&array[j + 1], &toInsert) < 0) { // once we find one element does not need to be moved, we can stop, because the array is monotonic because it is sorted
+        array[j] = array[j + 1]; // shift elements left, the other element(s) should come before the boundary point
+        j++;
+    }
+    array[j] = toInsert; // place the element at its correct position
+}
+
+void adjustPointPositionBack(Point *array, int n_points, int start_index) {
+    // move the point at start_index to its correct position to maintain sorted order
+    Point toInsert = array[start_index];
+    int j = start_index;
+    //similarly, j > 1 ensures it doesn't reach the first index [j will end at 1 after checking if 2 should swap with 1], which while it wouldn't throw off the front position such that the adjustFront method doesn't work because that has already been called,
+    //it is beneficial not to check the first index because it is a pointless computation. we can guarentee it will not shift.
+    while (j > 1 && comparePoints(&array[j - 1], &toInsert) > 0) { // once we find one element does not need to be moved, we can stop, because the array is monotonic because it is sorted
+        array[j] = array[j - 1]; // shift elements right, the other element(s) should come after the boundary point
+        j--;
+    } 
+    array[j] = toInsert; // place the element at its correct position
+}
+
+
