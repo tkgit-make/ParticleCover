293e804322d9ec4e04e814102320f9f2e9354e82
David Raibaut <draibaut@icloud.com>
2024-10-11 23:57:12 -0400

    First commit

--- python/matthew.py
+++
@@ -1,9 +0,0 @@
-from src.readers.reader import * 
-from src.coverers.data_structs import * 
-from src.coverers.line import * 
-from src.coverers.wedgecover import *
-from src.testers.test import *
-from src.debug import * 
-import matplotlib.pyplot as plt 
-import time 
-import ast
--- python/zakk.py
+++
@@ -1,6 +0,0 @@
-from  src.testers.test import *
-
-#wedge_test(movie = True, savefig=True)
-#wedge_test()
-wedge_test(movie = True)
-#wedge_test(movie = True, savefig=True)
